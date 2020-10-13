c893fcefd2e18f0bb0d45117125c1346d9edf87a
Ana Carolina Cortez Alves <carolina.cortez@aluno.ifsp.edu.br>
2020-10-13 18:03:10 -0300

    formatando o mapa na pg

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,1 +1,24 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const map = L.map('mapid').setView([-23.5428164,-46.6416237], 12)
+
+L
+.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+.addTo(map)
+
+const icon = L.icon({
+    iconUrl: './public/images/map-marker.svg',
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170,2]
+})
+
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das crianças <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
+
+L
+.marker([-23.5428164,-46.6416237], {icon: icon})
+.addTo(map)
+.bindPopup(popup)
