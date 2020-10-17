06250b0046ce4a32b8eb394576b510a1fa77a67e
Ana Carolina Cortez Alves <carolina.cortez@aluno.ifsp.edu.br>
2020-10-16 21:44:37 -0300

    add db

---
+++ src/database/db.js
@@ -0,0 +1,19 @@
+const database = require('sqlite-async')
+
+module.exports = database.open(__dirname + '/database.sqlite').then(function(db) {
+    return db.exec(`
+        CREATE TABLE IF NOT EXISTS orphanages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            lat TEXT,
+            lng TEXT,
+            name TEXT,
+            about TEXT,
+            whatsapp TEXT,
+            images TEXT,
+            instructions TEXT,
+            opening_hours TEXT,
+            open_on_weekends TEXT
+        );
+    `)
+})
+
---
+++ src/database/test.js
@@ -0,0 +1,32 @@
+const database = require('./db.js')
+const saveOrphanage = require('./saveOrphanage.js')
+
+database.then(async (db) => {
+
+    await saveOrphanage(db, {
+        lat: "-23.5378438",
+        lng: "-46.6518204",
+        name: "Great Home",
+        about: "Lar temporário para crianças em situação de risco e/ou vulnerabilidade social",
+        whatsapp: "1198123-4567",
+        images: [
+            "https://images.pexels.com/photos/1815257/pexels-photo-1815257.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            "https://images.pexels.com/photos/1148998/pexels-photo-1148998.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            "https://images.pexels.com/photos/1212805/pexels-photo-1212805.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            "https://images.pexels.com/photos/1449934/pexels-photo-1449934.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            "https://images.pexels.com/photos/256468/pexels-photo-256468.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            "https://images.pexels.com/photos/50581/girls-children-kids-friends-50581.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        ].toString(),
+        instructions: "Venha com bastante paciência e carinho, além de muita disposição para brincar, contar historinhas e se divertir",
+        opening_hours: "Das 15h às 17h",
+        open_on_weekends: "1"
+    })
+    
+    const allOprhanages = await db.all('SELECT * FROM orphanages')
+    console.log(allOprhanages)
+
+    const selectedOprhanages = await db.all('SELECT * FROM orphanages WHERE id = 1')
+    //console.log(selectedOprhanages)
+
+    
+})
